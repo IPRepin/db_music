524ef0df4ea9fe494b32799c24e829c68df45831
Pavel Repin <sghcd2012@yandex.ru>
2022-11-10 12:04:22 +0300

    INSERT INTO music_album

--- INSERT INTO music.sql
+++ INSERT INTO music.sql
@@ -12,7 +12,18 @@ VALUES
 (2, 'Baskov'),
 (3, 'Kirkorov'),
 (4, 'Shufutinsky'),
-(5, 'Kalinov mast')
-(6, 'Ariya')
-(7, 'Pesnyari')
+(5, 'Kalinov mast'),
+(6, 'Ariya'),
+(7, 'Pesnyari'),
 (8, 'ABBA')
+
+INSERT INTO music_album (music_album_id, year_of_release, name)
+VALUES
+(1, '2016-01-01', 'Hardwired...To Self-Destruct'),
+(2, '2009-01-01', 'Тебе одной'),
+(3, '2001-01-01', 'Magico Amor'),
+(4, '2007-01-01', 'Москва-Владивосток'),
+(5, '2018-01-01', 'Даурия'),
+(6, '2001-01-01', 'Химера'),
+(7, '1994-01-01', 'Песняры-25'),
+(8, '1986-01-01', 'ABBA Live')
