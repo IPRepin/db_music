73804fa6a276a04bcb7e946003e9413dea773e0d
Pavel Repin <sghcd2012@yandex.ru>
2022-11-11 11:46:08 +0300

    Update SELECT_2.sql

--- SELECT_2.sql
+++ SELECT_2.sql
@@ -5,3 +5,11 @@ SELECT COUNT(tracks_id) FROM tracks
 WHERE music_album_id IN(
     SELECT tracks_id FROM music_album
 	WHERE year_of_release BETWEEN '2019-01-01' AND '2020-01-01');
+
+SELECT music_album_id, AVG(duration) FROM tracks
+GROUP BY music_album_id;
+
+SELECT DISTINCT p.name FROM performers AS p
+LEFT JOIN album_performers AS ap ON p.performers_id = ap.performers_id
+LEFT JOIN music_album AS ma ON ap.performers_id = ma.music_album_id
+WHERE year_of_release < '2020-01-01';
